ac70a9c07fb8cfe1b1bb5b4e2e9aaffed895b3a3
Alex <alx_s@hotmail.fr>
2020-07-22 12:08:23 +0300

    show time of the message

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,30 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const adminBot = "Admin Bot";
+
 // run when client connects
 io.on("connection", (socket) => {
 	// Welcome current user
-	socket.emit("message", "Welcome to Realtime Chat app!");
+	socket.emit(
+		"message",
+		formatMessage(adminBot, "Welcome to Realtime Chat app!")
+	);
 
 	// Broadcast when a user connects
-	socket.broadcast.emit("message", "A user has joined the chat");
+	socket.broadcast.emit(
+		"message",
+		formatMessage(adminBot, "A user has joined the chat")
+	);
 
 	// Runs when client disconnects
 	socket.on("disconnect", () => {
-		io.emit("message", "A user has left the chat");
+		io.emit("message", formatMessage(adminBot, "A user has left the chat"));
 	});
 
 	// Listen for chatMessage
 	socket.on("chatMessage", (msg) => {
-		io.emit("message", msg);
+		io.emit("message", formatMessage("USER", msg));
 	});
 
 	// Broadcast to everyone
